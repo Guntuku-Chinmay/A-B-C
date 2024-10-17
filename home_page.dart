b777f6d114c9cb4eb9b5b7417046464735afe5e8
Guntuku-Chinmay <chinmayguntuku@gmail.com>
2024-10-17 21:03:18 +0530

    Added Navigation Bar

--- home_page.dart
+++ home_page.dart
@@ -11,6 +11,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String? _selectedValue;
+  int _currentIndex = 0; // Define _currentIndex for BottomNavigationBar
 
   @override
   void initState() {
@@ -18,25 +19,73 @@ class _MyHomePageState extends State<MyHomePage> {
     _selectedValue = null;
   }
 
+  final List<Widget> _children = [
+    Center(child: Text('Home Page')),
+    Center(child: Text('Daily Planner Page')),
+    Center(child: Text('Explore Page')),
+    Center(child: Text('Chat Page')),
+    Center(child: Text('Post Page')),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color(0xFF007BFF),
         leading: IconButton(
           icon: Icon(Icons.menu),
-          onPressed: () {}, // Add your menu button press logic here
+          onPressed: () {
+            // Add your menu button press logic here
+          },
         ),
         title: Text('Welcome'),
         actions: [
           IconButton(
             icon: Icon(Icons.person),
-            onPressed: () {}, // Add your profile button press logic here
+            onPressed: () {
+              // Add your profile button press logic here
+            },
           ),
         ],
       ),
-      body: Center(
-        child: Text('Home Page'),
+      body: _children[_currentIndex], // Display the selected page content
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Daily Planner',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore),
+            label: 'Explore',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chat',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.post_add),
+            label: 'Post',
+          ),
+        ],
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: MyHomePage(title: 'Welcome'),
+  ));
 }
